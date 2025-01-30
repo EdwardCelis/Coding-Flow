dca7025cdaace00ab8ec4e7627ca9e7426bc1d79
EdwardCelis <edwardcelis@live.com>
2025-01-30 12:06:39 -0500

    Update

--- BestNames-Instructor/main.py
+++ BestNames-Instructor/main.py
@@ -1,5 +1,12 @@
+# your first for loop: can you guess what it does?
 
-names_i_like = ['Thomson', 'Thomson2', 'Haddock', 'Snowy']
+names_i_like = ['Thomson', 'Thompson2', 'Haddock', 'Snowy']
 
 for name in names_i_like:
-    print(f"i like this name: {name}")
+    print(f"I like this name: {name}")
+
+for letter in "Jedidiah":
+    print(letter) 
+
+for number in range(1,10):
+    print(number)
